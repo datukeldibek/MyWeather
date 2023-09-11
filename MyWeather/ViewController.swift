7c746d1db041c80c840424e8fe3a9328f59f39b3
datukeldibek <118820830+datukeldibek@users.noreply.github.com>
2023-09-11 19:07:41 +0600

    Initial Commit

---
+++ MyWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyWeather
+//
+//  Created by Jarae on 11/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
